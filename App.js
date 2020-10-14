04d1cdc371c6d870b8ae2e7339ff898c2f0f0706
Max Peterson <maxpeterson00@gmail.com>
2020-10-14 19:26:50 -0400

    created example styled components

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import StyledSafeAreaView from './styles/styled-safe-area-view';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
+    <StyledSafeAreaView>
+      <Text>Replace this text component with your screen component!</Text>
       <StatusBar style="auto" />
-    </View>
+    </StyledSafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
