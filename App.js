63e0f6e35815b4c424b36bc668fff07bf1ed8773
Max Peterson <maxpeterson00@gmail.com>
2020-10-14 19:40:52 -0400

    move styled safe area view to app.js

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text } from 'react-native';
-import StyledSafeAreaView from './styles/styled-safe-area-view';
+import styled from 'styled-components/native'
+import { StatusBar } from 'expo-status-bar';
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  flex: 1;
+`
 
 export default function App() {
   return (
