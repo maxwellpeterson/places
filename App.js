423ff41739ff6405c386c216eb6852582bcc2165
jueunkangg <jueunkang593@gmail.com>
2020-10-14 17:10:04 -0700

    added back & follow buttons and some of the text (username text, full name text, bio text) that should be on the profile page

--- App.js
+++ App.js
@@ -1,13 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image, ScrollView, SafeAreaView, Alert, Button, TouchableOpacity} from 'react-native';
+
+const AppButton = ({onPress, title}) => (
+  <TouchableOpacity
+  onPress={onPress}
+  style={styles.buttonContainer}>
+    <Text style={styles.buttonText}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
+
+const BackButton = ({onPress, title}) => (
+  <TouchableOpacity
+  onPress={onPress}
+  style={styles.buttonContainer}>
+    <Text style={styles.buttonText}>
+      {title}
+    </Text>
+  </TouchableOpacity>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+        
+          <View style={{flexDirection: 'row'}}>
+            <View style={styles.backButtonScreenContainer}>
+              <BackButton title="back" size="sm"/>
+            </View>
+            <Text style={styles.headerText}>username</Text>
+          </View>
+
+          <Text style={styles.profileName}>first name last name</Text>
+
+          <View style={{flexDirection: 'row'}}>
+            <View style={styles.backButtonScreenContainer}>
+              <BackButton title="follow" size="sm"/>
+            </View><View style={styles.backButtonScreenContainer}>
+              <BackButton title="follow" size="sm"/>
+            </View>
+          </View>
+
+          <Text style={styles.profileBioText}>add a bio here</Text>
+
+          <View style={styles.buttonScreenContainer}>
+            <AppButton title="follow" size="sm"/>
+          </View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -18,4 +64,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonScreenContainer: {
+    flex: 1,
+    justifyContent: "start",
+    padding: 14
+  },
+  backButtonScreenContainer: {
+    flex: 1,
+    justifyContent: "start",
+    padding: 14
+  },
+  buttonContainer: {
+    elevation: 8,
+    backgroundColor: "#D3D3D3",
+    borderRadius: 8,
+    paddingVertical: 7,
+    paddingHorizontal: 14
+  },
+  buttonText : {
+    fontSize: 12,
+    color: "#fff",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    padding: 12
+  },
+  profileName: {
+    fontWeight: "bold"
+  },
+  profileBioText: {
+    fontSize: 12
+  }
 });
