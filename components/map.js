3b2aa36c2b2594bf673379ee10b363dc90ae425c
Max Peterson <maxpeterson00@gmail.com>
2020-10-21 19:52:38 -0400

    added nested callouts to markers for future customization

--- components/map.js
+++ components/map.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import styled from 'styled-components';
-
-const StyledMapView = styled(MapView)`
-  
-`
+import React from "react"
+import { StyleSheet, View, Text } from "react-native"
+import MapView from "react-native-maps"
+import { Marker, Callout } from "react-native-maps"
+import styled from "styled-components"
 
 // Need to figure out how to control the size of the map, not sure if it can be styled like a normal view
 
 export default function Map({ markers }) {
-  return(
-    <MapView style={{...StyleSheet.absoluteFillObject}}>
+  return (
+    <MapView style={{ ...StyleSheet.absoluteFillObject }}>
       {markers.map((marker) => (
-          <Marker
-            key={marker.title}
-            coordinate={marker.latlng}
-            title={marker.title}
-          />
-        ))}
+        <Marker key={marker.title} coordinate={marker.latlng}>
+          <Callout onPress={() => alert(`You selected ${marker.title}!`)}>
+            <Text>{marker.title}</Text>
+          </Callout>
+        </Marker>
+      ))}
     </MapView>
-  );
+  )
 }
