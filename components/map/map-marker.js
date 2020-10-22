d3b49cec2950ce2db4d2c3bfddb3d75f2e21431a
Max Peterson <maxpeterson00@gmail.com>
2020-10-21 23:04:44 -0400

    added more detailled callout styling

--- components/map/map-marker.js
+++ components/map/map-marker.js
@@ -1,12 +1,31 @@
 import React from "react"
-import { Text } from "react-native"
+import { Text, Dimensions } from "react-native"
 import { Marker, Callout } from "react-native-maps"
+import styled from "styled-components"
 
-export default function MapMarker({ title, latlng }) {
+const CalloutContainer = styled.View`
+  max-width: ${Dimensions.get("window").width / 2}px;
+`
+
+const CalloutTitle = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`
+
+const TitleWrapper = styled.View`
+  padding-bottom: 3px;
+`
+
+export default function MapMarker({ latlng, title, description }) {
   return (
     <Marker coordinate={latlng}>
       <Callout onPress={() => alert(`You selected ${title}!`)}>
-        <Text>{title}</Text>
+        <CalloutContainer>
+          <TitleWrapper>
+            <CalloutTitle>{title}</CalloutTitle>
+          </TitleWrapper>
+          <Text>{description}</Text>
+        </CalloutContainer>
       </Callout>
     </Marker>
   )
