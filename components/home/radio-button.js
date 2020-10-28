6442e7382af2b305405f0a5456f49d0afc29ce90
Max Peterson <maxpeterson00@gmail.com>
2020-10-27 21:25:42 -0400

    implemented basic radio button

---
+++ components/home/radio-button.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { View } from "react-native"
+import styled from "styled-components/native"
+
+const Border = styled.View`
+  width: 24px;
+  height: 24px;
+  border-radius: 12px;
+  border-width: 2px;
+  border-color: ${(props) => props.color || "black"};
+  justify-content: center;
+  align-items: center;
+`
+
+const Selector = styled.View`
+  width: 12px;
+  height: 12px;
+  border-radius: 12px;
+  background-color: ${(props) => props.color || "black"};
+`
+
+export default function RadioButton({ color, isSelected }) {
+  return (
+    <Border color={color}>{isSelected && <Selector color={color} />}</Border>
+  )
+}
