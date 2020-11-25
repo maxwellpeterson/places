e5eebcc70618a098bdf9a9cfba1b0bae3e8f16d8
Max Peterson <maxpeterson00@gmail.com>
2020-11-24 21:55:12 -0500

    set up some super basic routing

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,37 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
+const express = require("express")
+const cors = require("cors")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("./permissions.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://places-proto.firebaseio.com",
+})
+
+const db = admin.firestore()
+const app = express()
+app.use(cors({ origin: true }))
+
+app.get("/hello-world", (req, res) => {
+  return res.status(200).send("Hello World!")
+})
+
+// Does a thing!
+app.post("/api/create", (req, res) => {
+  ;(async () => {
+    try {
+      await db
+        .collection("places")
+        .doc("/" + req.body.id + "/")
+        .create({ title: req.body.title })
+      return res.status(200).send()
+    } catch (error) {
+      console.log(error)
+      return res.status(500).send(error)
+    }
+  })()
+})
+
+exports.app = functions.https.onRequest(app)
